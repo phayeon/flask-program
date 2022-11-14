991b1a52462a0fa8bd93043fb7c3de37a0fa541a
phayeon <phayeon1222@gmail.com>
2022-11-14 17:56:40 +0900

    11-14 17:56

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,46 +1,22 @@
-from ml.views import StrokeController
-from util.common import Common
+from ml.stroke import STROKE_MENUS, stroke_menu
+from ml.stroke import StrokeService
+
+
+def my_menu(ls):
+    for i, j in enumerate(ls):
+        print(f"{i}. {j}")
+    return input('메뉴선택: ')
+
 
 if __name__ == '__main__':
-
-    api = StrokeController()
-
-    STROKE_MENUS = ["종료",  # 0
-                    "데이터 구하기",  # 1
-                    "타깃 변수 설정",  # 2
-                    "데이터 처리",  # 3
-                    "시각화",  # 4
-                    "모델링",  # 5
-                    "학습",  # 6
-                    "예측"]  # 7
-
-    stroke_meta = {'id': '아이디', 'gender': '성별', 'age': '나이', 'hypertension': '고혈압', 'heart_disease': '심장병',
-                   'ever_married': '기혼 여부', 'work_type': '직종', 'Residence_type': '거주 형태', 'avg_glucose_level': '평균 혈당',
-                   'bmi': '비만도', 'smoking_status': '흡연 여부', 'stroke': '뇌졸중'}
-
+    t = StrokeService()
     while True:
-        menu = Common.menu(STROKE_MENUS)
-        if menu == "0":
+        menu = my_menu(STROKE_MENUS)
+        if menu == '0':
+            print("종료")
             break
-        elif menu == "1":
-            print(" ### 문제 제기 ###")
-            api.menu_1()
-
-        elif menu == "2":
-            print(" ### 데이터 구하기 ###")
-            api.menu_2()
-
-        elif menu == "3":
-            print(" ### 타깃 변수 설정 ###")
-            api.menu_3()
-
-        elif menu == "4":
-            print(" ### 데이터 처리 ###")
-            api.menu_4()
-
-        elif menu == "5":
-            print(" ### 시각화 ###")
-            api.menu_5()
-
         else:
-            print("다시 입력 해 주세요.")
+            try:
+                stroke_menu[menu](t)
+            except KeyError:
+                print(" ### Error ### ")
--- ml/stroke.py
+++ ml/stroke.py
@@ -1,4 +1,34 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+
+STROKE_MENUS = ["종료",  # 0
+                "데이터 구조 파악",  # 1
+                "변수 한글화",  # 2
+                "구간 변수 편집",  # 3 18세 이상만 사용함
+                "범주형 변수 편집",  # 4
+                "시각화",  # 5
+                "모델링",  # 6
+                "학습",  # 7
+                "예측"]  # 8
+
+stroke_meta = {
+    'id': '아이디', 'gender': '성별', 'age': '나이',
+    'hypertension': '고혈압',
+    'heart_disease': '심장병',
+    'ever_married': '기혼 여부',
+    'work_type': '직종',
+    'Residence_type': '거주 형태',
+    'avg_glucose_level': '평균 혈당',
+    'bmi': '체질량 지수',
+    'smoking_status': '흡연 여부',
+    'stroke': '뇌졸중'
+}
+stroke_menu = {
+    "1": lambda x: x.spec(),
+    "2": lambda x: x.rename_meta(),
+    "3": lambda x: x.interval_variables(),
+    "4": lambda x: x.categorical_variables(),
+}
 
 '''
 Data columns (total 12 columns):
@@ -23,6 +53,75 @@ dtypes: float64(3), int64(4), object(5)
 class StrokeService:
     def __init__(self):
         self.stroke = pd.read_csv('./data/healthcare-dataset-stroke-data.csv')
+        self.my_stroke = self.stroke.rename(columns=stroke_meta)
+        self.adult_stroke = self.stroke.rename(columns=stroke_meta)
 
-    def stroke_read(self):
-        return self.stroke
+    '''
+   1.스펙 보기
+   '''
+
+    def spec(self):
+        print(" --- 1.Shape ---")
+        print(self.stroke.shape)
+        print(" --- 2.Features ---")
+        print(self.stroke.columns)
+        print(" --- 3.Info ---")
+        print(self.stroke.info())
+        print(" --- 4.Case Top1 ---")
+        print(self.stroke.head(1))
+        print(" --- 5.Case Bottom1 ---")
+        print(self.stroke.tail(3))
+        print(" --- 6.Describe ---")
+        print(self.stroke.describe())
+        print(" --- 7.Describe All ---")
+        print(self.stroke.describe(include='all'))
+
+    '''
+   2.한글 메타 데이터
+   '''
+
+    def rename_meta(self):
+        print(" --- 2.Features ---")
+        print(self.my_stroke.columns)
+
+    '''
+    3.타깃 변수(= 종속 변수 dependent, Y값) 설정
+    입력 변수(= 설명 변수, 확률 변수, X값)
+    타깃 변수명: stroke (=뇌졸중)
+    타깃 변수값: 과거에 한 번이라도 뇌졸중이 발병했으면 1, 아니면 0
+    인터벌 = ['나이','평균 혈당','체질량 지수']
+    '''
+
+    def interval_variables(self):
+        interval = ['나이', '평균 혈당', '체질량 지수']
+        print(f'--- 구간 변수 타입 ---\n{self.my_stroke[interval].dtypes}')
+        stroke1 = self.my_stroke[self.my_stroke['나이'] > 18]
+        print(f'--- 결측 값을 가진 column ---\n{stroke1.isna().any()[lambda x: x]}')
+        print(f"--- 체질량 결측값 비율 확인 ---\n{self.my_stroke['체질량 지수'].isnull().mean()}")
+        print(f'고유 값 개수 : {len(pd.unique(self.my_stroke["아이디"]))}')
+        pd.options.display.float_format = '{:.2f}'.format
+        print(f'--- 구간 변수 기초 통계량 --- \n{self.my_stroke[interval].describe()}')
+        criterion = self.my_stroke['나이'] > 18
+        self.adult_stoke = self.my_stroke[criterion]
+        print(f'--- 성인 객체 스펙 --- \n{self.adult_stoke.shape}')
+        # 평균 혈당 232.64이하와 체질량 지수 60.3이하를 이상치로 규정 하고 제거함
+        self.adult_stoke
+        c1 = self.adult_stoke['평균 혈당'] <= 232.64
+        c2 = self.adult_stoke['체질량 지수'] <= 60.3
+        self.adult_stoke = self.adult_stoke[c1 & c2]
+        print(f'--- 이상치 제거한 성인 객체 스펙 ---\n{self.adult_stoke.shape}')
+
+    '''
+    4.범주형 = ['성별', '심장병', '기혼 여부', '직종', '거주 형태', '흡연 여부', '뇌졸중']
+    '''
+
+    def categorical_variables(self):
+        self.interval_variables()
+        category = ['성별', '심장병', '기혼 여부', '직종', '거주 형태', '흡연 여부', '고혈압']
+        print(f'--- 범주형 변수 데이터 타입 ---\n{self.adult_stroke[category].dtypes}')
+        print(f'--- 범주형 변수 결측값 ---\n{self.adult_stroke[category].isnull().sum()}')
+        print(f'--- 결측 값 있는 변수 ---\n{self.adult_stroke[category].isna().any()[lambda x: x]}')
+        self.stroke = self.adult_stroke
+        self.spec()
+        self.stroke.to_csv('./save/stroke.csv')
+        print(" ### 프리 프로세스 종료 ### ")
